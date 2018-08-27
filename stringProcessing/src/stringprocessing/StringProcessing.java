8f00800413375f0cf68a041d068466bebd39646f
Markus Wilson <mawilson1@aggies.ncat.edu>
2018-08-27 16:39:11 -0400

    finished level 1

--- stringProcessing/src/stringprocessing/StringProcessing.java
+++ stringProcessing/src/stringprocessing/StringProcessing.java
@@ -17,45 +17,12 @@ public class StringProcessing {
     
     public static void fileReader(String filename, String[] records)
     {
-        int currIndex=0;
-        /*String[] recFirst=new String[records.length];
-        String[] recLast=new String[records.length];
-        String[] recGender=new String[records.length];
-        int[] recAge=new int[records.length];
-        String[] recPhone=new String[records.length];*/
-        String[] recEmail=new String[records.length];
-        try {
-            Scanner reader =new Scanner(new File(filename));
-            
-
-            while(reader.hasNext())
-            {
-                String[] line = reader.nextLine().split(",");
-                /*recFirst[currIndex]=line[0];
-                recLast[currIndex]=line[1];
-                recGender[currIndex]=line[2];
-                recAge[currIndex]=Integer.parseInt(line[3]);
-                recPhone[currIndex]=line[4];*/
-                recEmail[currIndex]=line[5];
-                currIndex++;
-                //lineParse(records);
-            }
-            
-        } 
-        catch (FileNotFoundException ex) {
-            System.err.println("Unable to read file");
-        }
-        //valRecFirst(recFirst);
-        displayData(recEmail);
+        System.out.print("FIXME");
     }
-    public static String[] valRecFirst(String[] recFirst)
+    public static String valFirst(String recFirst)
     {
-        String[] valFirst = new String[recFirst.length];
-        for(int i=0; i<=recFirst.length;i++)
-        {
-            String toUpperCase = recFirst[i].toUpperCase();
-        }
-        return valFirst;
+        String upperVal= recFirst.toUpperCase();
+        if upperVal
     }
     public static void displayData(String []records)
     {
@@ -77,7 +44,35 @@ public class StringProcessing {
         // TODO code application logic here
         String file= "records.txt";
         String[] records= new String [100];
-        fileReader(file, records);
+        try 
+        {
+            Scanner reader =new Scanner(new File(args[0]));
+            int currIndex=0;
+            while(reader.hasNext())
+            {
+                String[] line = reader.nextLine().split(",");
+                String recFirst=line[0];
+                String recLast=line[1];
+                String recGender=line[2];
+                String recAge=line[3];
+                String recPhone=line[4];
+                String recEmail=line[5];
+                
+                records[currIndex]=String.format("%-20s%-10s%10s%15s%20s%30s",recFirst,recLast,recGender,recAge,recPhone,recEmail);
+                if(validFirst(recFirst))
+                {
+                    System.out.println(records[currIndex]);
+                }
+                
+                currIndex++;
+                
+            }
+        }
+        catch (FileNotFoundException ex) 
+        {
+            System.err.println("Unable to read file");
+        }
+        
         
         
     }
